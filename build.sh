ce5fd54c1964b3bec87e3afa6bf5131c467b8da2
Pavel Popov <pavelpopov@outlook.com>
2016-03-15 23:02:31 +0300

    Add missing vendor packages

--- build.sh
+++ build.sh
@@ -8,14 +8,15 @@ function log() {
     echo "[BUILD] $level: $msg"
 }
 
-OUT_DIR=./build
+OUT_DIR=$(pwd)/build
 [ ! -d $OUT_DIR ] && mkdir $OUT_DIR
 
 log "building app in docker container"
 docker run --rm \
-       -v .:/go/src/github.com/schmooser/go-mailer \
-       -v "$OUT_DIR":/go/bin \
-       --env CGO_ENABLED=0 \
-       --env GOOS=linux \
-       golang:latest go build -a -installsuffix cgo -v -o /go/bin/mailer
+    -v $(pwd):/go/src/github.com/schmooser/go-mailer \
+    -v "$OUT_DIR":/go/bin \
+    --env CGO_ENABLED=0 \
+    --env GOOS=linux \
+    --workdir /go/src/github.com/schmooser/go-mailer \
+    golang:latest go build -a -installsuffix cgo -v -o /go/bin/mailer
 log "app is built successfully"
