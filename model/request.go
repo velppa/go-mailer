c1e05a2aa1ab45da892bde3d2a09ea1a8029ec13
Pavel Popov <pavel@vio.com>
2026-07-23 19:13:42 +0200

    wip: sync before machine switch

--- model/request.go
+++ model/request.go
@@ -1,9 +1,27 @@
 package model
 
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log/slog"
+)
+
 // SendRequest is a body of POST /send request.
 type SendRequest struct {
-	MJML    string
+	MJML string
 	Message
+}
+
+func (sr SendRequest) GetReader() io.Reader {
+	b, err := json.Marshal(sr)
+	if err != nil {
+		slog.
+			With("module", "go-mailer/model").
+			Error("failed to marshal", "value", sr, "err", err)
+		return nil
+	}
+	return bytes.NewReader(b)
 }
 
 // Response is a response body from the API.
